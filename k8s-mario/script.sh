73466e802be1a6a0e7b4881f5a6db0d76e1761fc
s viswanath <s.viswanath57@gmail.com>
2024-04-06 18:29:41 +0530

    mario game repo

--- k8s-mario/script.sh
+++ k8s-mario/script.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # Install Terraform
-sudo yum install wget -y
-wget -O- https://yum.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://yum.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/yum/sources.list.d/hashicorp.list
-sudo yum update && sudo yum install terraform -y
+sudo apt install wget -y
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
 
 # Install kubectl
-sudo yum update
-sudo yum install curl -y
+sudo apt update
+sudo apt install curl -y
 curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 # Install AWS CLI 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-sudo yum-get install unzip -y
+sudo apt-get install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
 #Install docker
-sudo yum-get install docker.io -y
+sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 sudo reboot
 
