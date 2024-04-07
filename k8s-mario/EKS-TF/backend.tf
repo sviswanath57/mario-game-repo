610fdb80be4bb8d18198afa0e2fab86a364e6c2a
s viswanath <s.viswanath57@gmail.com>
2024-04-07 11:05:29 +0530

    mario game repo

--- k8s-mario/EKS-TF/backend.tf
+++ k8s-mario/EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 # terraform {
 #   backend "s3" {
-#     bucket = "Abhishek-mario878676" # Replace with your actual S3 bucket name
+#     bucket = "viswa-mario878676" # Replace with your actual S3 bucket name
 #     key    = "EKS/terraform.tfstate"
 #     region = "us-east-1"
 #   }
